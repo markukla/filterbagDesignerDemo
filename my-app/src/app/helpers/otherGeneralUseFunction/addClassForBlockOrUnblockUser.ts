6c5e33160f1075750dd8a5364d29a458359d611c
Grzegorz Dziedzic <grzegorzdziedzic@onet.pl>
2021-05-31 20:16:32 +0200

    styled all partners components

--- my-app/src/app/helpers/otherGeneralUseFunction/addClassForBlockOrUnblockUser.ts
+++ my-app/src/app/helpers/otherGeneralUseFunction/addClassForBlockOrUnblockUser.ts
@@ -1,11 +1,14 @@
 
 const addBlockedUserClass= (button: HTMLButtonElement)=>{
   button.classList.remove('userActivetedButton');
-  button.classList.add('userBlockedButton')
-
+  button.classList.add('userBlockedButton');
+  button.classList.add('icon');
+  button.classList.add('action-button');
 }
 const addActiveUserClass= (button: HTMLButtonElement)=>{
   button.classList.remove('userBlockedButton');
   button.classList.add('userActivetedButton');
+  button.classList.add('icon');
+  button.classList.add('action-button');
 }
 export {addBlockedUserClass,addActiveUserClass};
